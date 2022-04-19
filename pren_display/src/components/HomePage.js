cb3893fca89574cc0a0fd1f6f14d0d532f90e0b2
shadi omar <shadi.omar@powercoders.org>
2022-04-19 18:19:25 +0200

    adjust hte  fontsize of the body paragraphs

--- pren_display/src/components/HomePage.js
+++ pren_display/src/components/HomePage.js
@@ -1,26 +1,29 @@
 import * as React from 'react';
 import { Avatar, Container, Grid, Typography, Paper, Button, Box} from '@mui/material';
 import { borderRadius, color, margin, padding } from '@mui/system';
+import Image from 'material-ui-image';
 
-import Hslu from '../images/hsluCampus1.jpg';
-import Robot from '../images/robot.jpg'; import Robot2 from '../images/Elektro.jpg';
 import './NavBar.css';
+
 
 const styles = {
     paperContainer1: {
         height: 500,
+        Width: '100%',
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        backgroundImage: `url(${Hslu})`,
-        filter: "brightness(45%)",
-        filter: "contrast(80%)"
+        backgroundImage: `url(${"/static/hsluCampus2.jpg"})`,
+        filter: "brightness(250%)",
+        filter: "contrast(125%)",
+        objectFit: 'cover',
     },
 
     paperContainer2: {
         height: 500,
         backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundImage: `url(${Robot})`,
+        backgroundSize: "contain",
+        backgroundImage: `url(${"/static/robot.jpg"})`,
+        display: "block"
     },
 
     paperContainer3: {
@@ -28,22 +31,24 @@ const styles = {
         Width: 500,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        backgroundImage: `url(${Robot2})`,
+        backgroundImage: `url(${"/static/Elektro.jpg"})`,
     },
 };
 
 export default class Home extends React.Component {
     render() {
         return (
-            <box maxWidth="xl">
-                <Grid style={styles.paperContainer1} direction="row" spacing={1}>
-                    <Typography variant= '3' className='backgroundText'>Pflanzenerkennung ganz Autonom</Typography>
-                    <Typography variant="overline" className='titleSpan'></Typography>
+            <box maxWidth="xl" textAlign="center">
+                <Grid container justifyContent="center" style={styles.paperContainer1} direction="row" spacing={1}>
+                    <Typography variant= '3' className='backgroundText'
+                     >Pflanzenerkennung ganz Autonom</Typography>
                 </Grid>
+                
                 <Grid container spacing={0} row sx={{mr: 16, py: 8, my: 8}}>
-                    <Grid item xs={12} md={6}>
-                        <Typography variant='h1' class='projekt-discreption'>Unser Projekt</Typography>
-                        <Typography className='projekt-discreption'>
+                    <Grid container justifyContent='center' item xs={12} md={6}>
+                        <Typography variant='h1' class='projekt-discreption' id="unser">Unser Projekt</Typography>
+                        <Typography className='projekt-discreption' sx={{ fontSize: 22,
+                                lineHeight: 1.6}}>
                         Ich habe immer ein Projekt, an dem ich arbeite
                         Meistens für meine Kunden. Von Zeit zu Zeit ist es
                         aber auch ein persönliches Vorhaben, das mich neue
@@ -55,7 +60,7 @@ export default class Home extends React.Component {
                     </Grid>
                 </Grid>
 
-                <Grid container gridRow={2}  sx={{
+                <Grid container row={2}  sx={{
                     bgcolor: '#6d8a70',  color: 'white',
                     fontFamily: 'News Cycle,sans-serif',
                     mb: 9
@@ -84,25 +89,27 @@ export default class Home extends React.Component {
                     </Grid>
                 </Grid>
 
-                <box container display="block">
-                    <Typography variant='h5' sx={{
-                    textAlign: 'center',
-                    pb: 6,
-                    m: 2,
-                }}>Galerie</Typography>
-                    <Typography  sx={{
-                        textAlign: 'center',
-                        fontSize: 22,
-                        lineHeight: 1.6,
-                        color: '#323335', fontWeight: 'bold',
-                        mx: 20, mb: 5, p: 2, fontFamily: 'Catamaran, sans-serif'
-                    }}>Sie sind neugierig auf uns und unsere Arbeit?
-                        Schauen Sie sich in unserer Galerie um und bekommen Sie
-                        ein Gefühl für unsere Werke und was unsere Kunden daran so fasziniert.
-                        Wenn Sie nicht das finden, was Sie suchen, melden Sie sich bitte bei
-                        uns - wir helfen Ihnen gerne weiter.
-                    </Typography>
-                    <Grid container justifyContent="center">
+                <Grid container display={'inline-flex'} row>
+                    <Grid item container justifyContent="center" justifyItems= 'center' xs={'12'}>
+                        <Typography variant='h5' sx={{
+                            textAlign: 'center',
+                            pb: 6, m: 2,
+                            fontSize: 32
+                            }}>Galerie
+                        </Typography>
+                        <Typography
+                            sx={{
+                                textAlign: 'center',
+                                fontSize: 22,
+                                lineHeight: 1.6,
+                                color: '#323335',
+                                mb: 5, p: 2, fontFamily: 'Catamaran, sans-serif'
+                            }}>Sie sind neugierig auf uns und unsere Arbeit?
+                            Schauen Sie sich in unserer Galerie um und bekommen Sie
+                            ein Gefühl für unsere Werke und was unsere Kunden daran so fasziniert.
+                            Wenn Sie nicht das finden, was Sie suchen, melden Sie sich bitte bei
+                            uns - wir helfen Ihnen gerne weiter.
+                        </Typography>
                         <Button variant="contained"
                             sx={{
                                 color: 'white',
@@ -116,8 +123,7 @@ export default class Home extends React.Component {
                             }}>zur Galerie
                         </Button>
                     </Grid>
-                    
-                </box>
+                </Grid>
             </box>           
         )
     }
