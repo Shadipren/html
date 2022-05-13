fa6fb09589c6ae04108aec00ffe7926013802b0d
shadi omar <shadi.omar@powercoders.org>
2022-05-13 14:33:47 +0200

    change footer color to light gray and delete blank line spaces.

--- pren_display/src/components/Footer.js
+++ pren_display/src/components/Footer.js
@@ -1,10 +1,8 @@
 import { Box, Typography, Grid} from "@mui/material";
 
-
-
 const Footer = (props) => {
     return (<>
-        <Box sx={{position: "fixed", bottom: 0, height: "auto", width: "100vw", borderTop: 1, borderColor: '#d2d2d9', backgroundColor:"#e0e0e0"  }} component="footer">
+        <Box sx={{position: "fixed", bottom: 0, height: "auto", width: "100vw", borderTop: 1, borderColor: '#d2d2d9', backgroundColor:"#eeeeee"  }} component="footer">
             <Grid container>
                 <Grid item xs={12} lg={4}>
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Marvin Schlüssel</Typography>
@@ -19,11 +17,9 @@ const Footer = (props) => {
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Giulio Meuli</Typography>
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Shadi Omar</Typography>
                 </Grid>
-            
             </Grid>
         </Box>
     </>)
 }
-
 
 export default Footer;
