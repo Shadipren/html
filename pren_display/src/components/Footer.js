daee6e3327b00ace4b6ef17439ce70ad9ef2d40c
shadi omar <shadi.omar@powercoders.org>
2022-05-19 10:52:09 +0200

    make font size by footer smaller.

--- pren_display/src/components/Footer.js
+++ pren_display/src/components/Footer.js
@@ -5,17 +5,17 @@ const Footer = (props) => {
         <Box sx={{position: "relative", bottom: 0, height: "auto", width: "100vw", borderTop: 1, borderColor: '#d2d2d9', backgroundColor:"#eeeeee"  }} component="footer">
             <Grid container>
                 <Grid item xs={12} lg={4}>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Marvin Schlüssel</Typography>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Gabriel Waldvogel</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" align="center" gutterBottom>Marvin Schlüssel</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" align="center" gutterBottom>Gabriel Waldvogel</Typography>
                 </Grid>
                 <Grid item xs={12} lg={3}>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Andrin Witschi</Typography>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Manuel Roos</Typography>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Corsin Kirchhofer</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" align="center" gutterBottom>Andrin Witschi</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" align="center" gutterBottom>Manuel Roos</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" align="center" gutterBottom>Corsin Kirchhofer</Typography>
                 </Grid>
                 <Grid item xs={12} lg={3}>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Giulio Meuli</Typography>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Shadi Omar</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" align="center" gutterBottom>Giulio Meuli</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" align="center" gutterBottom>Shadi Omar</Typography>
                 </Grid>
             </Grid>
         </Box>
