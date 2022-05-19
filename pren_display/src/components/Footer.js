0f060ba10796ea0e55f02958f93d1b11d29b31ef
Corsin Kirchhofer <cxultimate@gmail.com>
2022-05-19 10:36:12 +0200

    footer not sticky anymore and changed one name to be centered

--- pren_display/src/components/Footer.js
+++ pren_display/src/components/Footer.js
@@ -2,16 +2,16 @@ import { Box, Typography, Grid} from "@mui/material";
 
 const Footer = (props) => {
     return (<>
-        <Box sx={{position: "fixed", bottom: 0, height: "auto", width: "100vw", borderTop: 1, borderColor: '#d2d2d9', backgroundColor:"#eeeeee"  }} component="footer">
+        <Box sx={{position: "relative", bottom: 0, height: "auto", width: "100vw", borderTop: 1, borderColor: '#d2d2d9', backgroundColor:"#eeeeee"  }} component="footer">
             <Grid container>
                 <Grid item xs={12} lg={4}>
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Marvin Schlüssel</Typography>
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Gabriel Waldvogel</Typography>
-                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Corsin Kirchhofer</Typography>
                 </Grid>
                 <Grid item xs={12} lg={3}>
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Andrin Witschi</Typography>
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Manuel Roos</Typography>
+                    <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Corsin Kirchhofer</Typography>
                 </Grid>
                 <Grid item xs={12} lg={3}>
                     <Typography className="footerTypo" sx={{pt:"5px"}} fontWeight="light" variant="h6" align="center" gutterBottom>Giulio Meuli</Typography>
