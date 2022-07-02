9d1192594e78eb9be718680b0493f87a0d5b9eb8
Corsin Kirchhofer <cxultimate@gmail.com>
2022-07-02 11:25:17 +0200

    removed coils 3 & 4 because they dont exist anymore

--- pren_display/src/components/Coils.js
+++ pren_display/src/components/Coils.js
@@ -7,15 +7,15 @@ const CoilsTable = () => {
 
     const [coil1, setCoil1] = React.useState([]);
     const [coil2, setCoil2] = React.useState([]);
-    const [coil3, setCoil3] = React.useState([]);
-    const [coil4, setCoil4] = React.useState([]);
+    // const [coil3, setCoil3] = React.useState([]);
+    // const [coil4, setCoil4] = React.useState([]);
 
     useEffect(() => {
         socket.on('coils', (data) => {
             setCoil1(data[0]);
             setCoil2(data[1]);
-            setCoil3(data[2]);
-            setCoil4(data[3]);
+            // setCoil3(data[2]);
+            // setCoil4(data[3]);
         })
     }, [socket]);
 
@@ -27,15 +27,15 @@ const CoilsTable = () => {
     const rowCoi = [
         { id: "1", nrCoil: coil1.nr_coil, data: coil1.value },
         { id: "2", nrCoil: coil2.nr_coil, data: coil2.value },
-        { id: "3", nrCoil: coil3.nr_coil, data: coil3.value },
-        { id: "4", nrCoil: coil4.nr_coil, data: coil4.value },
+        // { id: "3", nrCoil: coil3.nr_coil, data: coil3.value },
+        // { id: "4", nrCoil: coil4.nr_coil, data: coil4.value },
     ];
 
     return (
         <DataGrid
             rows={rowCoi}
             columns={colCoi}
-            pageSize={4}
+            pageSize={2}
             rowsPerPageOptions={[]}
             hideFooter={true}
             hideFooterPagination={true}
